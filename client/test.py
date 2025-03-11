ea32eb118a7c62c6832d8956272b30cd8e07c572
ZiarayZ <ziarayz@gmail.com>
2025-03-11 16:21:37 +0000

    comments on plans

---
+++ client/test.py
@@ -0,0 +1,10 @@
+# async function for each "game" loop:
+# menu loop
+# gameplay loop
+# pause loop
+
+# these loops shouldn't "affect" each other much and can be ran simultaneously
+
+# classes and succession? entity, block, projectile, etc
+
+# sprites or draw? since stick figures can be constructed out of shapes and animated
